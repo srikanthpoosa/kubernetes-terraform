d36d9c5998571394fe3ce2a8028c3e48cf052674
srikanthpoosa <77892649+srikanthpoosa@users.noreply.github.com>
2022-04-14 11:57:44 +0530

    nodecount increased to 2

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_kubernetes_cluster" "terraform_k8s" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = 2
     vm_size    = "Standard_B2s"
   }
 
