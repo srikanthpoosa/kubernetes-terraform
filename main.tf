3488f5a0bb3184b75a4b7b27ccefde0d3f05ea37
srikanthpoosa <77892649+srikanthpoosa@users.noreply.github.com>
2022-04-14 08:18:25 +0530

    created k8s terraform

---
+++ main.tf
@@ -0,0 +1,35 @@
+resource "azurerm_resource_group" "resource_group" {
+  name     = "TF-group"
+  location = "SouthIndia"
+}
+
+resource "azurerm_kubernetes_cluster" "terraform_k8s" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.terraform_k8s.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.terraform_k8s.kube_config_raw
+
+  sensitive = true
+}
